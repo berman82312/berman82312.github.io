d446d5df303e1a6f2fccbb657c330479fb760d05
詹小魚 <berman82312@gmail.com>
2020-10-06 16:52:32 +0800

    Restructure directory.

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  from: undefined,
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: {
+    content: ['./themes/tree/layout/**/*.ejs'],
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
