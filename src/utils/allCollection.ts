579b39422c951f431d6901acc78673b69170eb06
Littlefish <berman82312@gmail.com>
2024-03-22 11:04:15 +0800

    Fix: type checks

--- src/utils/allCollection.ts
+++ src/utils/allCollection.ts
@@ -2,10 +2,12 @@ import { getCollection } from "astro:content";
 import type { CollectionEntry } from "astro:content";
 
 export type AllCollectionEntry = CollectionEntry<"posts" | "movies">;
+
 const modifySlug = (name: string) => (entry: AllCollectionEntry) => ({
   ...entry,
   slug: `/${name}/` + entry.slug,
 });
+
 export const allPosts = [
   ...(await getCollection("posts")).map(modifySlug("posts")),
   ...(await getCollection("movies")).map(modifySlug("movies")),
